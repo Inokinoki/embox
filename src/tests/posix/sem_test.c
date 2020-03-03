3768ece40af0eb6dce66f458c022b9fc9690cb3e
Weixuan XIAO <veyx.shaw@gmail.com>
2020-03-03 22:05:22 +0100

    Add test for sem_post, sem_wait

--- src/tests/posix/sem_test.c
+++ src/tests/posix/sem_test.c
@@ -12,8 +12,7 @@
 
 EMBOX_TEST_SUITE("posix/semaphore test");
 
-#define SEMAPHORE_INIT_VALUE        0
-#define SEMAPHORE_INIT_MAX_VALUE    5
+#define SEMAPHORE_INIT_VALUE 5
 
 TEST_CASE("Test sem_init with shared=0, value=0") {
 	sem_t semaphore;
@@ -50,31 +49,47 @@ TEST_CASE("Test sem_getvalue") {
 	sem_t semaphore;
     int value = -1;
 
-	test_assert_equal(sem_init(&semaphore, 0, SEMAPHORE_INIT_MAX_VALUE), ENOERR);
+	test_assert_equal(sem_init(&semaphore, 0, SEMAPHORE_INIT_VALUE), ENOERR);
     test_assert_equal(value, -1);                       // Test before getvalue
     test_assert_equal(sem_getvalue(&semaphore, &value), ENOERR);
-    test_assert_equal(value, SEMAPHORE_INIT_MAX_VALUE); // Test after getvalue
+    test_assert_equal(value, SEMAPHORE_INIT_VALUE); // Test after getvalue
 }
 
-TEST_CASE("Test sem_getvalue with a sem_post") {
+TEST_CASE("Test sem_wait") {
 	sem_t semaphore;
     int value = -1;
 
-	test_assert_equal(sem_init(&semaphore, 0, SEMAPHORE_INIT_MAX_VALUE), ENOERR);
+	test_assert_equal(sem_init(&semaphore, 0, SEMAPHORE_INIT_VALUE), ENOERR);
     test_assert_equal(value, -1);
     test_assert_equal(sem_getvalue(&semaphore, &value), ENOERR);
-    test_assert_equal(value, SEMAPHORE_INIT_MAX_VALUE); // Test before sem_post
+    test_assert_equal(value, SEMAPHORE_INIT_VALUE); 	// Test before sem_wait
     
-    test_assert_equal(sem_post(&semaphore), ENOERR);
+    test_assert_equal(sem_wait(&semaphore), ENOERR);
     test_assert_equal(sem_getvalue(&semaphore, &value), ENOERR);
-    test_assert_equal(value, SEMAPHORE_INIT_MAX_VALUE); // Test after sem_post
+    test_assert_equal(value, SEMAPHORE_INIT_VALUE - 1); // Test after sem_wait
+}
+
+TEST_CASE("Test sem_post") {
+	sem_t semaphore;
+    int value = -1;
+
+	test_assert_equal(sem_init(&semaphore, 0, SEMAPHORE_INIT_VALUE), ENOERR);
+    test_assert_equal(value, -1);
+    test_assert_equal(sem_getvalue(&semaphore, &value), ENOERR);
+    test_assert_equal(value, SEMAPHORE_INIT_VALUE); 	// Test before sem_wait
+    
+    test_assert_equal(sem_wait(&semaphore), ENOERR);
+    test_assert_equal(sem_getvalue(&semaphore, &value), ENOERR);
+    test_assert_equal(value, SEMAPHORE_INIT_VALUE - 1); // Test after sem_wait
+
+	test_assert_equal(sem_post(&semaphore), ENOERR);
+    test_assert_equal(sem_getvalue(&semaphore, &value), ENOERR);
+    test_assert_equal(value, SEMAPHORE_INIT_VALUE); 	// Test after sem_post
 }
 
 /*
-extern int    sem_post(sem_t *);
 extern int    sem_timedwait(sem_t *, const struct timespec *); //this
 extern int    sem_trywait(sem_t *); //OK
-extern int    sem_wait(sem_t *);
 */
 
 /*
